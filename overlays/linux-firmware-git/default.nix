ab628fbd5ad9fbb1013d53aac300145d8c37e2d9
Onésime BERNARD <bernard.onesime@gmail.com>
2024-11-21 18:53:08 +0100

    update linux-firmare-git to 384e3

--- overlays/linux-firmware-git/default.nix
+++ overlays/linux-firmware-git/default.nix
@@ -1,10 +1,10 @@
 final: prev:
 {
   linux-firmware-git = prev.linux-firmware.overrideAttrs (oldAttrs: {
-    version = "ad74054cd57db9401caf9c1c0b340b667d322216";
+    version = "384e300b5571df30740c4be7becfd6a0922352e2";
     src = builtins.fetchGit {
       url = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
-      rev = "ad74054cd57db9401caf9c1c0b340b667d322216";
+      rev = "384e300b5571df30740c4be7becfd6a0922352e2";
     };
   });
 }
