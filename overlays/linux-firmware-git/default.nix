2f12b19c2dbe94108f4e63a8ea175555acd6f76d
Onésime BERNARD <bernard.onesime@gmail.com>
2024-12-08 15:43:41 +0100

    update linux-firmare-git to 7e1bc

--- overlays/linux-firmware-git/default.nix
+++ overlays/linux-firmware-git/default.nix
@@ -1,10 +1,10 @@
 final: prev:
 {
   linux-firmware-git = prev.linux-firmware.overrideAttrs (oldAttrs: {
-    version = "384e300b5571df30740c4be7becfd6a0922352e2";
+    version = "7e1bc11980a4528af56a91d7989ec75e3fea78e3";
     src = builtins.fetchGit {
       url = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
-      rev = "384e300b5571df30740c4be7becfd6a0922352e2";
+      rev = "7e1bc11980a4528af56a91d7989ec75e3fea78e3";
     };
   });
 }
