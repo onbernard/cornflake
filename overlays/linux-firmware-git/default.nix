3b1c121e64098334b44cb7cebfcd92ff80816b95
Onésime BERNARD <bernard.onesime@gmail.com>
2025-03-23 14:18:30 +0100

    update linux-firmware-git to 710e3

--- overlays/linux-firmware-git/default.nix
+++ overlays/linux-firmware-git/default.nix
@@ -1,10 +1,10 @@
 final: prev:
 {
   linux-firmware-git = prev.linux-firmware.overrideAttrs (oldAttrs: {
-    version = "bd3d8a8b1bb76e352ea8d60db8198f0b40ff9ac8";
+    version = "710a336b31981773a3a16e7909fd83daeaec9db1";
     src = builtins.fetchGit {
       url = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
-      rev = "bd3d8a8b1bb76e352ea8d60db8198f0b40ff9ac8";
+      rev = "710a336b31981773a3a16e7909fd83daeaec9db1";
     };
   });
 }
