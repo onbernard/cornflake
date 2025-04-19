325de118995f80a7afa2fb19a407ad950ba9fd74
Onésime BERNARD <bernard.onesime@gmail.com>
2025-04-19 17:56:40 +0200

    Add mojo package. Remove overlays from devShells

--- flake.nix
+++ flake.nix
@@ -3,25 +3,26 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";    
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ self, ... }:
-  with inputs;
-  let
-    linux-firmware-git = import ./overlays/linux-firmware-git;
-    intel-media-driver-git = import ./overlays/intel-media-driver-git;
-  in
-  {
-    overlays = {
-      default =  nixpkgs.lib.composeExtensions intel-media-driver-git linux-firmware-git;
-    };
-    devShells.x86_64-linux.default = 
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; overlays = [ self.overlays.default]; };
-    in
-    nixpkgs.legacyPackages.x86_64-linux.mkShell {
-      buildInputs = [ pkgs.linux-firmware-git pkgs.intel-media-driver-git ];
-    };
-  };
+  outputs = inputs @ {self, ...}:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+        mojo = pkgs.callPackage ./packages/mojo.nix {};
+        linux-firmware-git = import ./overlays/linux-firmware-git;
+        intel-media-driver-git = import ./overlays/intel-media-driver-git;
+      in {
+        packages.mojo = mojo;
+        overlays.default = inputs.nixpkgs.lib.composeExtensions intel-media-driver-git linux-firmware-git;
+        devShells.default = pkgs.mkShell {
+          packages = [mojo];
+        };
+      }
+    );
 }
