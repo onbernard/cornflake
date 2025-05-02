eab3ab97abea6c5a324a87759c449b4371b6fbc2
Onésime BERNARD <bernard.onesime@gmail.com>
2025-05-02 18:10:46 +0200

    Fix and update intel-media-driver-git

--- overlays/intel-media-driver-git/update.sh
+++ overlays/intel-media-driver-git/update.sh
@@ -5,7 +5,7 @@ set -eu -o pipefail
 
 repo="https://github.com/intel/media-driver.git"
 
-revision="$(git ls-remote --refs --tags --sort refname $repo | grep -E "intel-media-[0-9]+\.[0-9]+\.[0.9]+" | tail -n1 | cut -f2 | cut -d '/' -f3 | cut -d '-' -f3)"
+revision="$(git ls-remote --refs --tags --sort refname $repo | grep -E "intel-media-[0-9]+\.[0-9]+\.[0-9]+" | tail -n1 | cut -f2 | cut -d '/' -f3 | cut -d '-' -f3)"
 
 sed -i -E "s/(version\s*=\s*\").*(\";)/\1$revision\2/" ./default.nix
 
